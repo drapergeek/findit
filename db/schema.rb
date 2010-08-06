7e98a27baabf7a2ba33e2e7665bde79b5bdf4db4
Jason Draper <aethiolas@gmail.com>
2010-08-06 11:31:38 -0400

    added the ability to add a user and define if they can log in or not

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100805203137) do
+ActiveRecord::Schema.define(:version => 20100806152143) do
 
   create_table "announcements", :force => true do |t|
     t.string   "name"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(:version => 20100805203137) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "can_login"
   end
 
 end
