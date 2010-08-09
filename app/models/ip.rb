477fd14df87099eebdb347af86ddfad4bcfc8fb8
Jason Draper <aethiolas@gmail.com>
2010-08-09 09:32:47 -0400

    changed name to number to fix error and required number

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,6 +1,7 @@
 class Ip < ActiveRecord::Base
   attr_accessible :number, :info, :building_id
   validates_presence_of :building_id
+  validates_presence_of :number
   belongs_to :item
   belongs_to :building
   named_scope :unassigned, :conditions=>['item_id IS NULL']
