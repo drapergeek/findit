16c2bfa6bcbbbfd28bb5c4966feb49b433b0c8b4
Jason Draper <aethiolas@gmail.com>
2010-08-09 16:12:27 -0400

    added the location to items

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessible :name, :info, :building_id
   validates_presence_of :building_id
   belongs_to :building
+  has_many :items
+  def full_name
+    building.name + " - " + name
+  end
 end
