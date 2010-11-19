db2cf5f6c39d7dec4eca2c7f11b1ad501a663605
Jason Draper <gdraper@vt.edu>
2010-11-19 16:58:18 -0500

    added searching and pagination the system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
     title ||=column.titleize
     css_class = column == sort_column ? "currecnt #{sort_direction}" :nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort=>column, :direction=>direction}, {:class=>css_class}
-    
+    link_to title, params.merge(:sort=>column, :direction=>direction, :page=>nil), {:class=>css_class}
   end
 end
