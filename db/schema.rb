766932c7f7ca1ddfd6097dabeefc00bfcb0d4599
Jason Draper <jason@drapergeek.com>
2011-01-02 11:18:30 -0500

    added the critical and priority field

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101004191754) do
+ActiveRecord::Schema.define(:version => 20110102161014) do
 
   create_table "announcements", :force => true do |t|
     t.string   "name"
@@ -74,6 +75,8 @@ ActiveRecord::Schema.define(:version => 20101004191754) do
     t.text     "info"
     t.boolean  "in_use"
     t.datetime "surplused_at"
+    t.boolean  "critical"
+    t.integer  "priority"
   end
 
   create_table "locations", :force => true do |t|
