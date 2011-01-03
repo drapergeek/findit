c3b8ef2fc5fdfc048e93374eb8a59b94a715754a
Jason Draper <gdraper@vt.edu>
2011-01-03 15:07:21 -0500

    changed the OS to sort by name

--- app/models/operating_system.rb
+++ app/models/operating_system.rb
@@ -2,4 +2,5 @@ class OperatingSystem < ActiveRecord::Base
   attr_accessible :name, :info
   has_many :items, :dependent=>:nullify
   has_many :softwares, :dependent=>:nullify
+  default_scope :order=>['name']
 end
