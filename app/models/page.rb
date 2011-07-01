9c8a5f74cb8b250294d3bfd27b47142f8336a153
Jason Draper <jason@drapergeek.com>
2011-07-01 12:17:08 -0400

    cleaned up old files

--- app/models/page.rb
+++
@@ -1,3 +0,0 @@
-class Page < ActiveRecord::Base
-  attr_accessible :title, :body
-end
--- test/unit/announcement_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AnnouncementTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Announcement.new.valid?
-  end
-end
