3f0c259a515e6e645eb684d2b3bb8c33676add5a
Jason Draper <jason@drapergeek.com>
2011-11-04 15:20:45 -0400

    trying to use the yaml and see what that guy is

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,36 +7,38 @@ class ApplicationController < ActionController::Base
   before_filter :set_login
   before_filter :check_for_valid_login
   helper_method :current_user
-  
-  
+
+
   private 
 
 
   def current_user
     return @current_user if defined?(@current_user)
     @current_user = User.find_by_login(session[:cas_user])
-    unless @current_user.can_login == true
-      @current_user = nil
-    end
-    unless @current_user.nil?
-      @current_user.last_login_ip = request.remote_ip 
-      @current_user.last_login = Time.now
-      @current_user.save!
+    if @current_user
+      unless @current_user.can_login == true
+        @current_user = nil
+      end
+      unless @current_user.nil?
+        @current_user.last_login_ip = request.remote_ip 
+        @current_user.last_login = Time.now
+        @current_user.save!
+      end
     end
   end
-  
+
   def set_login
     if Rails.env != "production"
       session[:cas_user] = "gdraper"
     end
   end
-  
-  
+
+
   def check_for_valid_login
     unless current_user
       #this user can't be found so get them out of here!
       CASClient::Frameworks::Rails::Filter.logout(self)
     end
   end
-  
+
 end
