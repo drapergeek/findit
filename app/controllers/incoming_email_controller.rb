82c23871beaf4dcb6491955f0b77f08eb9505d12
Jason Draper <jason@drapergeek.com>
2011-11-04 15:18:00 -0400

    trying to use the yaml and see what that guy is

--- app/controllers/incoming_email_controller.rb
+++ app/controllers/incoming_email_controller.rb
@@ -5,6 +5,10 @@ class IncomingEmailController < ApplicationController
   def index
     logger.info "I got the email"
     logger.info params[:mail].class
-    logger.info params[:mail].to_yaml
+    logger.info Rails.env
+    mail = Mail.read(params[:mail])
+    logger.info mail.class
+    logger.info mail.to_s
+    redirect_to root_url
   end
 end
