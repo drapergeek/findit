be8205cf18b3c85d47533dea0eee5833e21c535a
Jason Draper <jason@drapergeek.com>
2011-11-04 15:04:27 -0400

    trying another way

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,5 +1,5 @@
 class BuildingsController < ApplicationController
-  prepend_before_filter CASClient::Frameworks::Rails::Filter
+  prepend_before_filter { |nothing|  CASClient::Frameworks::Rails::Filter if Rails.env=="production" }
   def index
     @buildings = Building.all
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    prepend_before_filter CASClient::Frameworks::Rails::Filter
+    prepend_before_filter { |nothing|  CASClient::Frameworks::Rails::Filter if Rails.env=="production" }
   def index
     @users = User.order "last_name"
   end
