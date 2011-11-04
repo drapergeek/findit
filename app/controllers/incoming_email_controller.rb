8ce748b513f14ef98c64cf3d17ae746bd56fcdc8
Jason Draper <jason@drapergeek.com>
2011-11-04 15:09:02 -0400

    trying to parse the email

--- app/controllers/incoming_email_controller.rb
+++ app/controllers/incoming_email_controller.rb
@@ -1,10 +1,13 @@
 class IncomingEmailController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authentictity_token
   skip_before_filter :check_for_valid_login
 
   def index
-    logger.info "Got the incoming email!"
-    logger.info params
+    logger.info "I got the email"
+    mail = Mail.read(params[:mail])
+    logger.info mail.from.address
+    logger.info mail.subject
+    logger.info mail.body.decoded
     redirect_to root_url
   end
 end
