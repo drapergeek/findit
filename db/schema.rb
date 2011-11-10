7c207929fb7b6b215bd96cb7a3d69df57c668ad2
Brent Montague <bigbam505@gmail.com>
2011-11-10 16:23:33 -0500

    Reply comment field added to comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111109182141) do
+ActiveRecord::Schema.define(:version => 20111110210452) do
 
   create_table "areas", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20111109182141) do
     t.integer  "ticket_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "reply"
   end
 
   create_table "dns_names", :force => true do |t|
