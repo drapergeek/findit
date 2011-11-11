485b0e89975446a4460beb4aa4526ff739f41b6d
Brent Montague <bigbam505@gmail.com>
2011-11-11 12:30:51 -0500

    updated emailer but an error exists in creating a ticket we need to look at

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,5 +6,5 @@ ActionMailer::Base.smtp_settings = {
   :domain               =>"recsports.vt.edu"
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000" if Rails.env.development?
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
