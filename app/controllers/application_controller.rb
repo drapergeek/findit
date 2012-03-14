e8040e71866ca9c74fa0f3a0831e8c06fb99b459
Brent Montague <bigbam505@gmail.com>
2012-03-14 15:56:28 -0400

    Added handling when an item isnt found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,25 +7,29 @@ class ApplicationController < ActionController::Base
   before_filter :check_for_valid_login
   helper_method :current_user
 
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    redirect_to root_url, :notice => exception.message
+  end
+
   private 
 
   def current_user
-   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def check_for_valid_login
     if Rails.env !="production"
-        session[:user_id] = User.find_by_login(APP_CONFIG['default_user'])
-        current_user
+      session[:user_id] = User.find_by_login(APP_CONFIG['default_user'])
+      current_user
     end
     ip = request.remote_ip
     unless current_user
-     #this user can't be found so get them out of here!
-     logger.info "Current user can't be found for some reason..."
-     session[:user_id] = nil
-     flash[:notice] = "You must be signed in to view this page"
-     redirect_to no_auth_path
-     return
+      #this user can't be found so get them out of here!
+      logger.info "Current user can't be found for some reason..."
+      session[:user_id] = nil
+      flash[:notice] = "You must be signed in to view this page"
+      redirect_to no_auth_path
+      return
     end
     logger.info "User: #{current_user.login} IP: #{ip}"
   end
