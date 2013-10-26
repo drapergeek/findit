55aa9d5c2a00e1e15ef5f2c4fe45510c3a6912c3
Jason Draper <jason@drapergeek.com>
2013-10-26 16:29:33 -0400

    Add the db extensions.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130929175343) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "areas", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
