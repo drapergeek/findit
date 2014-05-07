2613a9386d6ee7113484653e24a9b5dc378b7617
Jason Mabry <jmabry@mymabry.com>
2014-05-07 15:30:11 -0400

    Move company name to ENV

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -4,6 +4,7 @@ class LabelsController < ApplicationController
   def show
     @item = Item.friendly.find(params[:id])
     @image_url = ENV['LOGO_IMAGE_URL']
+    @company_name = ENV['COMPANY_NAME']
     render 'show'
   end
 end
