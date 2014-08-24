7af7c6fab715d5e41adfc1245eba4a0a6ee67ff7
Jason Draper <jason@drapergeek.com>
2014-08-24 10:12:35 -0700

    Rearrange the routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,49 @@
 Findit::Application.routes.draw do
   devise_for :users
-  root :to=>"items#index", :type=>"Desktop"
-  resources :users
-  resources :allowed_users, :only => :create
+  root to: 'items#index'
+
+  resources :allowed_users, only: :create
+  resources :areas, except: [:show]
+  resources :buildings, except: [:show]
   resources :comments
-  resources :areas, :except =>[:show]
-  resources :projects, :except => [:show]
-  resources :tickets do
-    collection do
-      post 'take'
-      get 'show_by_area'
-      get 'show_by_project'
-    end
-  end
-  resources :operating_systems, :except => [:show]
   resources :dns_names
-  resources :softwares
-  resources :locations, :except => [:show]
-  resources :buildings, :except => [:show]
+
   resources :items do
     collection do
-      get 'not_checked'
-      post 'add_ip'
-      get 'remove_ip'
-      get 'remove_dns_name'
-      get 'mark_as_inventoried'
-      get 'surplus'
+      get "not_checked"
+      post "add_ip"
+      get "remove_ip"
+      get "remove_dns_name"
+      get "mark_as_inventoried"
+      get "surplus"
     end
   end
-  resources :item_types, only: [:index, :create, :destroy]
 
-  resources :labels, only: [:show]
-
-  resources :ips, :except => [:show]
-  resources :pages
   resources :installations do
     collection do
-      post 'install_software'
-      get 'uninstall_software'
+      post "install_software"
+      get "uninstall_software"
     end
   end
+
+  resources :ips, except: [:show]
+  resources :item_types, only: [:index, :create, :destroy]
+  resources :labels, only: [:show]
+  resources :locations, except: [:show]
+  resources :operating_systems, except: [:show]
+  resources :pages
+  resources :projects, except: [:show]
+  resources :softwares
+
+  resources :tickets do
+    collection do
+      post "take"
+      get "show_by_area"
+      get "show_by_project"
+    end
+  end
+
+  resources :users
 
   namespace :api do
     resources :items, only: [:index, :show]
